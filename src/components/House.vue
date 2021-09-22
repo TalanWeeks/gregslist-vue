610a601bf6adb8019ef28789db935c79e5d7c15d
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-09-22 16:55:00 -0600

    complete with houses

--- src/components/House.vue
+++ src/components/House.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="mb-4 listing">
-    <router-link :to="{name: 'House', params: { houseId: house.id}}">
-      <div class="card selectable">
-        <img :src="house.img" alt="listing image" class="rounded">
-        <div class="card-body">
-          <h5 class="d-flex justy-content-between">
-            <span> Baths: {{ house.bathrooms }} Beds: {{ house.bedrooms }} Levels: {{ house.levels }}</span>
-            <span>${{ house.price }}</span>
-          </h5>
+  <div class="col-md-4">
+    <div class="mb-4 listing">
+      <router-link :to="{name: 'House', params: { houseId: house.id}}">
+        <div class="card shadow elevate-1 selectable">
+          <img :src="house.img" alt="listing image" class="rounded-top">
+          <div class="card-body">
+            <h5 class="d-flex justy-content-between">
+              <span> Baths: {{ house.bathrooms }} Beds: {{ house.bedrooms }} Levels: {{ house.levels }}</span>
+            </h5>
+            <span>Price: ${{ house.price }}</span>
+          </div>
         </div>
-      </div>
-    </router-link>
+      </router-link>
+    </div>
   </div>
 </template>
 
